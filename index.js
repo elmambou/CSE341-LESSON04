c34432cc3c8345e652c9a1afb28b65ad34de6d37
elmambou <esmambou@gmail.com>
2024-01-30 22:13:59 +0200

    first commit lesson 04 team assignment

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors'); // import cors module
+const app = express();
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+
+app
+  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+  .use(cors())
+  .use(express.json())
+  .use(express.urlencoded({ extended: true }))
+  .use('/', require('./routes'));
+
+
+  const db = require('./models');
+  db.mongoose
+    .connect(db.url, {
+      useUnifiedTopology: true,
+      useUnifiedTopology: true,
+    }) // Removed useNewUrlParser option
+    .then(() => {
+      console.log('Connected to the database!');
+    })
+    .catch((err) => {
+      console.log('Cannot connect to the database!', err);
+      process.exit();
+    });
+
+
+
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
