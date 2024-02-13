a5884aceee0923354cb2cc5bb8ca812d944fd032
elmambou <esmambou@gmail.com>
2024-02-13 09:15:10 +0200

    changes to swagger.js

--- swagger.js
+++ swagger.js
@@ -1,18 +1,46 @@
 const swaggerAutogen = require('swagger-autogen')();
 
+//const doc = {
+  //info: {
+   // title: 'My API',
+    //description: 'Contact API'
+  //},
+  //host: 'localhost:8083', 
+  //schemes: ['http'],      
+//};
+
+//const outputFile = './swagger.json';
+//const endpointsFiles = ['./routes/index.js'];
+
 const doc = {
   info: {
     title: 'My API',
     description: 'Contact API'
   },
-  host: 'localhost:8083', 
-  schemes: ['http'],      
+  host: 'localhost:8083',
+  schemes: ['http'],
+  consumes: ['application/json'],
+  produces: ['application/json'],
+  tags: [
+    {
+      name: 'contacts',
+      description: 'Operations related to contacts'
+    }
+  ],
+  definitions: {
+    Contact: {
+      type: 'object',
+      properties: {
+        firstName: { type: 'string' },
+        lastName: { type: 'string' },
+        email: { type: 'string' },
+        favoriteColor: { type: 'string' },
+        birthday: { type: 'string' }
+      },
+      required: ['firstName', 'lastName', 'email', 'favoriteColor']
+    }
+  }
 };
-
-const outputFile = './swagger.json';
-const endpointsFiles = ['./routes/index.js'];
 
 // generate swagger.json
 swaggerAutogen(outputFile, endpointsFiles, doc);
-
-
