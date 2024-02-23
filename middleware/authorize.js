45e1d6501735df7c0739a8fb76630d7532e234a0
elmambou <esmambou@gmail.com>
2024-02-23 23:08:30 +0200

    changes to middleware contacts

--- middleware/authorize.js
+++ middleware/authorize.js
@@ -2,7 +2,7 @@ const checkLogin = (req, res, next) => {
     if (!req.oidc.isAuthenticated()) {
       return res.status(401).send({
         error: 'Not authorized to change data without login.',
-        login: "https://cse341-l7.onrender.com/login"
+        login: "https://lesson04-cse341.onrender.com/login"
       });
     }
     next();
