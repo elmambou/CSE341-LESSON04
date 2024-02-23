fdd382eb48e8ad78e0c07b8f04214ef86277a200
elmambou <esmambou@gmail.com>
2024-02-24 01:27:40 +0200

    changes to app.js

--- app.js
+++ app.js
@@ -64,8 +64,8 @@ app.get('/', (req, res) => {
 // })
 
 app.get('/profile', requiresAuth(), (req, res) => {
- // console.log(JSON.stringify(req.oidc.user))
-    res.send(JSON.stringify(req.oidc.user));
+  console.log(JSON.stringify(req.oidc.user))
+  res.send(JSON.stringify(req.oidc.user));
 }); 
 //End OAuth
 
