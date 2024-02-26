7c3d2de5310e5d12a39b03ff5b04ce8dd4b36e3b
elmambou <esmambou@gmail.com>
2024-02-26 10:50:13 +0200

    schema latest changes

--- schema.js
+++ schema.js
@@ -3,37 +3,22 @@ const {
     GraphQLString,
     GraphQLList,
     GraphQLSchema,
-    GraphQLNonNull,
-    GraphQLInputObjectType,
-    GraphQLID
+    GraphQLNonNull
   } = require('graphql');
 
-const { ObjectId } = require('mongodb'); // Import ObjectId from MongoDB package
-const Contact = require('./models/contacts'); // Adjust the import path as per your project structure
-
-// Define Contact input type
-const ContactInputType = new GraphQLInputObjectType({
-    name: 'ContactInput',
-    fields: () => ({
-        _id: { type: new GraphQLNonNull(GraphQLString) },
-        firstName: { type: new GraphQLNonNull(GraphQLString) },
-        lastName: { type: new GraphQLNonNull(GraphQLString) },
-        email: { type: new GraphQLNonNull(GraphQLString) },
-        favoriteColor: { type: new GraphQLNonNull(GraphQLString) },
-        birthday: { type: new GraphQLNonNull(GraphQLString) }
-    })
-});
+  const { ObjectId } = require('mongodb'); // Import ObjectId from MongoDB package
+  const Contact = require('./models/contacts')
 
 // Define Contact type
 const ContactType = new GraphQLObjectType({
     name: 'Contact',
     fields: () => ({
-        _id: { type: new GraphQLNonNull(GraphQLString) },
-        firstName: { type: new GraphQLNonNull(GraphQLString) },
-        lastName: { type: new GraphQLNonNull(GraphQLString) },
-        email: { type: new GraphQLNonNull(GraphQLString) },
-        favoriteColor: { type: new GraphQLNonNull(GraphQLString) },
-        birthday: { type: new GraphQLNonNull(GraphQLString) }
+        _id: { type: GraphQLString }, // Add _id field
+        firstName: { type: GraphQLString },
+        lastName: { type: GraphQLString },
+        email: { type: GraphQLString },
+        favoriteColor: { type: GraphQLString },
+        birthday: { type: GraphQLString }
     })
 });
 
@@ -41,29 +26,21 @@ const ContactType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
-        contacts: {
-            type: new GraphQLList(ContactType),
-            resolve: async (_, args, context) => {
-                try {
-                    // Logic to retrieve all contacts from MongoDB
-                    return await Contact.find();
-                } catch (err) {
-                    throw new Error(err.message);
-                }
-            }
-        },
         contact: {
             type: ContactType,
             args: {
-                id: { type: new GraphQLNonNull(GraphQLString) }
+                firstName: { type: GraphQLString }
             },
-            resolve: async (_, args, context) => {
-                try {
-                    // Logic to retrieve a single contact by ID from MongoDB
-                    return await Contact.findById(args.id);
-                } catch (err) {
-                    throw new Error(err.message);
-                }
+            resolve(parent, args, context) {
+                // Your logic to retrieve a single contact by firstName
+                // This could be querying from MongoDB using Mongoose
+            }
+        },
+        contacts: {
+            type: new GraphQLList(ContactType),
+            resolve(parent, args, context) {
+                // Logic to retrieve all contacts from MongoDB using Mongoose
+                return context.db.collection('contacts').find().toArray(); // Example assuming 'contacts' is your MongoDB collection
             }
         }
     }
@@ -76,33 +53,43 @@ const Mutation = new GraphQLObjectType({
         addContact: {
             type: ContactType,
             args: {
-                input: { type: new GraphQLNonNull(ContactInputType) }
+                firstName: { type: new GraphQLNonNull(GraphQLString) },
+                lastName: { type: new GraphQLNonNull(GraphQLString) },
+                email: { type:new GraphQLNonNull(GraphQLString) },
+                favoriteColor: { type:new GraphQLNonNull(GraphQLString) },
+                birthday: { type:new GraphQLNonNull(GraphQLString) }
             },
-            resolve: async (_, { input }, context) => {
-                try {
-                    // Logic to add a new contact to the database
-                    return await Contact.create(input);
-                } catch (err) {
-                    throw new Error(err.message);
-                }
+            resolve(parent, args, context) {
+                // Your logic to add a new contact to the database
+                // This could involve creating a new document in MongoDB using Mongoose
             }
         },
         updateContact: {
             type: ContactType,
             args: {
-                input: { type: new GraphQLNonNull(ContactInputType) }
+                _id: { type: new GraphQLNonNull(GraphQLString) },
+                firstName: { type: GraphQLString },
+                lastName: { type: GraphQLString },
+                email: { type: GraphQLString },
+                favoriteColor: { type: GraphQLString },
+                birthday: { type: GraphQLString }
             },
-            resolve: async (_, { input }, context) => {
-                try {
-                    const { _id, ...updateFields } = input;
-                    // Convert the _id argument to ObjectId
-                    const objectId = new ObjectId(_id);
-                    // Logic to update an existing contact in the database
-                    await Contact.updateOne({ _id: objectId }, { $set: updateFields });
-                    return await Contact.findById(objectId);
-                } catch (err) {
-                    throw new Error(err.message);
-                }
+            resolve(parent, args, context) {
+                const { db } = context;
+                const { _id, ...updateFields } = args;
+
+                // Convert the _id argument to ObjectId
+                const objectId = new ObjectId(_id);
+
+                return db.collection('contacts').updateOne(
+                    { _id: objectId },
+                    { $set: updateFields }
+                ).then(() => {
+                    // Return the updated contact
+                    return db.collection('contacts').findOne({ _id: objectId });
+                }).catch(err => {
+                    throw new Error('Failed to update contact');
+                });
             }
         }
     }
---
+++ schemaL2.js
@@ -0,0 +1,114 @@
+const { 
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLList,
+    GraphQLSchema,
+    GraphQLNonNull,
+    GraphQLInputObjectType,
+    GraphQLID
+  } = require('graphql');
+
+const { ObjectId } = require('mongodb'); // Import ObjectId from MongoDB package
+const Contact = require('./models/contacts'); // Adjust the import path as per your project structure
+
+// Define Contact input type
+const ContactInputType = new GraphQLInputObjectType({
+    name: 'ContactInput',
+    fields: () => ({
+        _id: { type: new GraphQLNonNull(GraphQLString) },
+        firstName: { type: new GraphQLNonNull(GraphQLString) },
+        lastName: { type: new GraphQLNonNull(GraphQLString) },
+        email: { type: new GraphQLNonNull(GraphQLString) },
+        favoriteColor: { type: new GraphQLNonNull(GraphQLString) },
+        birthday: { type: new GraphQLNonNull(GraphQLString) }
+    })
+});
+
+// Define Contact type
+const ContactType = new GraphQLObjectType({
+    name: 'Contact',
+    fields: () => ({
+        _id: { type: new GraphQLNonNull(GraphQLString) },
+        firstName: { type: new GraphQLNonNull(GraphQLString) },
+        lastName: { type: new GraphQLNonNull(GraphQLString) },
+        email: { type: new GraphQLNonNull(GraphQLString) },
+        favoriteColor: { type: new GraphQLNonNull(GraphQLString) },
+        birthday: { type: new GraphQLNonNull(GraphQLString) }
+    })
+});
+
+// Define Root Query
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        contacts: {
+            type: new GraphQLList(ContactType),
+            resolve: async (_, args, context) => {
+                try {
+                    // Logic to retrieve all contacts from MongoDB
+                    return await Contact.find();
+                } catch (err) {
+                    throw new Error(err.message);
+                }
+            }
+        },
+        contact: {
+            type: ContactType,
+            args: {
+                id: { type: new GraphQLNonNull(GraphQLString) }
+            },
+            resolve: async (_, args, context) => {
+                try {
+                    // Logic to retrieve a single contact by ID from MongoDB
+                    return await Contact.findById(args.id);
+                } catch (err) {
+                    throw new Error(err.message);
+                }
+            }
+        }
+    }
+});
+
+// Define Mutations
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addContact: {
+            type: ContactType,
+            args: {
+                input: { type: new GraphQLNonNull(ContactInputType) }
+            },
+            resolve: async (_, { input }, context) => {
+                try {
+                    // Logic to add a new contact to the database
+                    return await Contact.create(input);
+                } catch (err) {
+                    throw new Error(err.message);
+                }
+            }
+        },
+        updateContact: {
+            type: ContactType,
+            args: {
+                input: { type: new GraphQLNonNull(ContactInputType) }
+            },
+            resolve: async (_, { input }, context) => {
+                try {
+                    const { _id, ...updateFields } = input;
+                    // Convert the _id argument to ObjectId
+                    const objectId = new ObjectId(_id);
+                    // Logic to update an existing contact in the database
+                    await Contact.updateOne({ _id: objectId }, { $set: updateFields });
+                    return await Contact.findById(objectId);
+                } catch (err) {
+                    throw new Error(err.message);
+                }
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery,
+    mutation: Mutation
+});
